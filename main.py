16dbf8ad2991bfce6ca047c2e087e369a1a4bed1
marcheydecke <158345219+marcheydecke@users.noreply.github.com>
2024-10-31 12:38:24 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-index = 0
-
-def on_forever():
+def on_button_a():
     global index
     index += 1
     datalogger.include_timestamp(FlashLogTimeStampFormat.SECONDS)
@@ -9,4 +7,7 @@ def on_forever():
         datalogger.create_cv("B-Feld-X", input.magnetic_force(Dimension.STRENGTH)))
     datalogger.mirror_to_serial(True)
     basic.pause(5000)
-basic.forever(on_forever)
+input.on_button_event(Button.A, input.button_event_click(), on_button_a)
+
+index = 0
+index = 0
--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
-let index = 0
-basic.forever(function () {
-    index += 1
-    datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
-    datalogger.log(
-    datalogger.createCV("", index),
-    datalogger.createCV("Temperatur", input.temperature()),
-    datalogger.createCV("B-Feld-X", input.magneticForce(Dimension.Strength))
-    )
-    datalogger.mirrorToSerial(true)
-    basic.pause(5000)
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let index2 = 0; index2 < 12; index2++) {
+        index += 1
+        datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
+        datalogger.log(
+        datalogger.createCV("", index),
+        datalogger.createCV("Temperatur", input.temperature()),
+        datalogger.createCV("B-Feld-X", input.magneticForce(Dimension.Strength)),
+        datalogger.createCV("Lautstärke", input.soundLevel())
+        )
+        datalogger.mirrorToSerial(true)
+        basic.pause(5000)
+    }
+    basic.showString("END")
 })
+let index = 0
+index = 0
