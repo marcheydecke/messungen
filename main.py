a8a9ce0fc9218ceb3baf03458e37b94c54f3816c
marcheydecke <158345219+marcheydecke@users.noreply.github.com>
2024-10-31 13:22:28 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+index = 0
+
 def on_button_a():
     global index
-    index += 1
-    datalogger.include_timestamp(FlashLogTimeStampFormat.SECONDS)
-    datalogger.log(datalogger.create_cv("", index),
-        datalogger.create_cv("Temperatur", input.temperature()),
-        datalogger.create_cv("B-Feld-X", input.magnetic_force(Dimension.STRENGTH)))
-    datalogger.mirror_to_serial(True)
-    basic.pause(5000)
+    index = 0
+    datalogger.delete_log(datalogger.DeleteType.FAST)
+    for index2 in range(12):
+        index += 1
+        datalogger.include_timestamp(FlashLogTimeStampFormat.SECONDS)
+        datalogger.log(datalogger.create_cv("index", index),
+            datalogger.create_cv("Temperatur", input.temperature()),
+            datalogger.create_cv("B-Feld-X", input.magnetic_force(Dimension.STRENGTH)),
+            datalogger.create_cv("Lautstaerke", input.sound_level()))
+        datalogger.mirror_to_serial(True)
+        basic.show_string("" + str(index))
+        basic.pause(5000)
+    basic.show_string("END")
 input.on_button_event(Button.A, input.button_event_click(), on_button_a)
-
-index = 0
-index = 0
--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
+let index = 0
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    index = 0
+    datalogger.deleteLog(datalogger.DeleteType.Fast)
     for (let index2 = 0; index2 < 12; index2++) {
         index += 1
         datalogger.includeTimestamp(FlashLogTimeStampFormat.Seconds)
         datalogger.log(
-        datalogger.createCV("", index),
+        datalogger.createCV("index", index),
         datalogger.createCV("Temperatur", input.temperature()),
         datalogger.createCV("B-Feld-X", input.magneticForce(Dimension.Strength)),
-        datalogger.createCV("Lautstärke", input.soundLevel())
+        datalogger.createCV("Lautstaerke", input.soundLevel())
         )
         datalogger.mirrorToSerial(true)
+        basic.showString("" + index)
         basic.pause(5000)
     }
     basic.showString("END")
 })
-let index = 0
-index = 0
